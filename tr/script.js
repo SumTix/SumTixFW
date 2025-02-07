cf52af3eb82db3b196b3857c1567d5881b191d5a
Ali Asali <103429406+SumTix@users.noreply.github.com>
2025-02-07 12:57:39 +0300

    Add files via upload

---
+++ tr/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const menuLinks = document.getElementById('menu-links');
+    menuLinks.classList.toggle('active');
+});
