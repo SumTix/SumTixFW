23d5b23a1a8b8223f5760f638a0dbe1e2486cc64
Ali Asali <103429406+SumTix@users.noreply.github.com>
2025-05-01 15:02:07 +0300

    config.php

--- ar/config.php
+++ ar/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $dbname = "sumtix";
-$username = "root";
-$password = "";
+$username = "ss";
+$password = "558273";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
